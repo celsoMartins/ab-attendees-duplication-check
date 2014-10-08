3b253aa081e2e9fa0838e85db6c9ec7a48ed8efe
Celso Martins <celso.av.martins@gmail.com>
2014-10-07 22:58:32 -0300

    checking duplicates

--- src/br/com/ab/registrations/check/Check.java
+++ src/br/com/ab/registrations/check/Check.java
@@ -1,8 +1,22 @@
 package br.com.ab.registrations.check;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Check {
 
-	public void gimmeDuplicates() {
-		throw new IllegalArgumentException("Source is required");
+	private List<String> source = new ArrayList<String>();
+	private List<String> target = new ArrayList<String>();
+
+	public Check(List<String> source, List<String> target) {
+		this.source = source;
+		this.target = target;
+	}
+
+	public List<String> gimmeDuplicates() {
+		if (source.isEmpty() || target.isEmpty()) throw new IllegalArgumentException("Source and target are required");
+		List<String> intersection = new ArrayList<String>(source);
+		intersection.retainAll(target);
+		return intersection;
 	}
 }
--- src/br/com/ab/registrations/check/CheckTest.java
+++ src/br/com/ab/registrations/check/CheckTest.java
@@ -1,5 +1,11 @@
 package br.com.ab.registrations.check;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -12,8 +18,25 @@ public class CheckTest {
 	@Test
 	public void shouldNotCheckDuplicatesWithoutSource() {
 		expected.expect(IllegalArgumentException.class);
-		expected.expectMessage("Source is required");
-		Check check = new Check();
+		expected.expectMessage("Source and target are required");
+		Check check = new Check(new ArrayList<String>(), new ArrayList<String>());
 		check.gimmeDuplicates();
+	}
+	
+	@Test
+	public void shouldNotCheckDuplicatesWithoutTarget() {
+		expected.expect(IllegalArgumentException.class);
+		expected.expectMessage("Source and target are required");
+		List<String> source = new ArrayList<String>(Arrays.asList("A","B","C","D","E"));
+		Check check = new Check(source, new ArrayList<String>());
+		check.gimmeDuplicates();
+	}
+	
+	@Test
+	public void shouldReturnDuplicates() {
+		List<String> source = new ArrayList<String>(Arrays.asList("A","B","C","D","E"));
+		List<String> target = new ArrayList<String>(Arrays.asList("A","C","D","F"));
+		Check check = new Check(source, target);
+		assertEquals(new ArrayList<String>(Arrays.asList("A", "C", "D")), check.gimmeDuplicates());
 	}
 }
